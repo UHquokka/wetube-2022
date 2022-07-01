200efee7b41a4932a4ef186ac88ce84d5d07c142
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-07-01 21:38:50 +0900

    app.listen / 4000 port

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 //"express"에서 express를 import 해야한다. 
 
+const PORT = 4000;
+
 //1. express app을 만든다. app이란 이름은 관습임. 
 const app = express();
 
-const handleListening = () => console.log("Server listening on port 4000 🚀")
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
